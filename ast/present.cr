d454c4c95ff9f78c6df284a62ccdbe96b1f09621
Dragos Tiselice <dragostiselice@gmail.com>
2015-12-27 11:53:10 +0200

    Change Present addition to result in another Present, not Base.

--- ast/present.cr
+++ ast/present.cr
@@ -4,4 +4,12 @@ class Present < Base
   def initialize(@index = 0, @size = 0)
     @value = ""
   end
+
+  def +(other)
+    if other.is_a? Present
+      Present.new @index, @size + other.size
+    else
+      super.+(other)
+    end
+  end
 end
